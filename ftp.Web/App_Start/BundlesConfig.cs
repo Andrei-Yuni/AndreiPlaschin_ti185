d1d2a646ea0c999d21ad2e68281514dffa38a90c
Andrei <kerokar4@gmail.com>
2020-03-20 03:42:54 +0200

    Add project files.

---
+++ ftp.Web/App_Start/BundlesConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ftp.Web.App_Start
+{
+     static public class BundlesConfig
+     {
+          static public void RegisterBundles(BundleCollection bundles)
+          {
+               bundles.Add(new StyleBundle("~/bundles/css")
+                         .Include("~/Content/bootstrap.min.css")
+                         .Include("~/Content/mdb.min.css")
+                         .Include("~/Content/style.css"));
+               bundles.Add(new ScriptBundle("~/bundles/js")
+                            .Include("~/Scripts/jquery.min.js")
+                            .Include("~/Scripts/popper.min.js")
+                            .Include("~/Scripts/bootstrap.min.js")
+                            .Include("~/Scripts/mdb.min.js"));
+          }
+     }
+}
